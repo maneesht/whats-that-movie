7a74ec3b5be45026d2b4d6b925314e8e7cdbe960
Maneesh Tewani <mtewani@purdue.edu>
2015-11-07 18:27:40 -0500

    Configured file uploading

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-var express = require('express')
-    app = express();
+var express = require('express'),
+    app = express(),
+    multer = require('multer'),
+    upload = multer({dest: 'uploads/'}),
+    port = process.env.PORT || 8080;
+
 app.get('/', function(req, res) {
-    res.send("Hello World");
+    res.sendFile("index.html", {root: "www/"}); 
+});
+app.post('/upload', upload.single('avatar'), function(req, res) {
+    console.log(req.body);
+    console.log(req.file);
+    res.status(204).end();
 })
 app.listen("8080");
